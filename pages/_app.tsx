23064e72f9aa1d591c919885783abb985d9eb881
wsaada19 <willsaada19@gmail.com>
2024-08-13 16:57:30 -0400

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import '../styles/global.css';
 import { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
+const env = process.env.NODE_ENV;
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      {env === 'production' && <Analytics />}
     </>
   );
 }
