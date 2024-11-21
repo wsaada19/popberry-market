e87910c8c4a66b47b237562e7dd2ec64a0330921
wsaada19 <willsaada19@gmail.com>
2024-11-21 10:37:57 +0000

    make public and cleanup

---
+++ README.md
@@ -0,0 +1,3 @@
+### Pixels Guild War
+
+Simple nextjs web app for displaying player data for Pixels online events and guild wars
--- vitest.config.js
+++ vitest.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./test.setup.js'],
     include: [
       '**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
       '__tests__/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
